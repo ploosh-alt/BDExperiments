25834134bc258e615a385e9c0adca409a3067815
plooshi <95927318+plooshi@users.noreply.github.com>
2022-10-28 21:18:31 -0400

    Fix not loading because of local variables.

--- experiments.plugin.js
+++ experiments.plugin.js
@@ -14,13 +14,13 @@ module.exports = class {
 	start() {
 		let wpRequire;
 		window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
-		mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
-		usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
-		nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
+		let mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+		let usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
+		let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
 		try {
 		    nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 1}})
 		} catch (e) {}
-		oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+		let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
 		usermod.exports.default.__proto__.getCurrentUser = () => ({hasFlag: () => true})
 		nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
 		usermod.exports.default.__proto__.getCurrentUser = oldGetUser
